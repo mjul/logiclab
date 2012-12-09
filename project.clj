072ebd4300a37a1a2fd3f08a1db9426328eef7db
Martin Jul <martin@mjul.com>
2012-12-09 23:39:12 +0100

    Upgraded to Clojure 1.4, core.logic 0.7.5

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject logiclab "0.0.1-SNAPSHOT"
+(defproject logiclab "0.0.2-SNAPSHOT"
   :description "Experiments with core.logic programming."
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/core.logic "0.6.7"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/core.logic "0.7.5"]
                  [org.clojure/math.combinatorics "0.0.2"]])
